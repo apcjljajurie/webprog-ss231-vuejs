7fe2864d1aa34c64f4554019f7b623c360cf0ccb
ken-101 <111231705+ken-101@users.noreply.github.com>
2025-01-21 08:24:28 +0800

    done

---
+++ home/js/vue.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+     return {
+      message: "Hello WEBPROG SS231 Students!",
+      vueClass: "pinkBG"
+     }
+    }
+   })
+   
+   app.mount('#app')
+ 
