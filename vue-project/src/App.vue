e1e525f3adceaccf1981f6fb17aaef6465051594
ken-101 <111231705+ken-101@users.noreply.github.com>
2025-02-12 16:07:12 +0800

    separate the file
    the the code from app vue to country.vuw

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,29 +1,15 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const instruments = ref([])
-
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
-
-onMounted(() => {
-  getInstruments()
-})
+<script>
 </script>
 
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
-  <h1>Food Lists</h1>
+  
+  <h1>Food List</h1>
   <food-mango/>
   <food-strawberry/>
   <food-mango/>
   <food-strawberry/>
   <personal-profile/>
+  <country/>
 </template>
 <style>
 
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
+import Country from './components/Country.vue'
 
 const app = createApp(App)
 app.component('food-mango', FoodItem)
 app.component('food-strawberry', FoodItem2)
 app.component('personal-profile', PersonalProfile)
+app.component('country', Country)
 app.mount('#app')
